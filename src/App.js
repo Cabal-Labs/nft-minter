55020a6c3378d2541e5f105010f21881556cfebc
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-11-03 12:28:36 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
-import './Styles/App.css';
+import "./Styles/App.css";
 
-
-import MainBox from './components/MainBox';
-
-
-
-
+import MainBox from "./components/MainBox";
 
 function App() {
+	useEffect(() => {
+		document.body.style.overflow = "hidden";
+	}, []);
 
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-  },[])
-
-  
-
-  return (
-    
-        <div className="App" >
-          
-          <div> 
-            <Navbar/>
-          </div>
-      
-          <div >
-            <MainBox/>
-          </div>
-
-        </div>
-      
-        
-      
-    
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<h1>Cabal Minter</h1>
+			<MainBox />
+		</div>
+	);
 }
 
 export default App;
--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -1,165 +1,222 @@
 import React, { useState, useEffect } from "react";
-import "./../Styles/MainBox.css"
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
-import exampleIMG from "./../exampleIMG.json"
-import {NFTStorage, File} from "nft.storage";
-import {ethers} from "ethers"
-import AICabalNFT from "./../ABIs/AICabalNFT.json"
-import Carousel, {next} from 'react-material-ui-carousel'
-import { Paper, Button } from '@mui/material'
-import Typer from "./Typer.js"
-
-
+import "./../Styles/MainBox.css";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import exampleIMG from "./../exampleIMG.json";
+import { NFTStorage, File } from "nft.storage";
+import { ethers } from "ethers";
+import AICabalNFT from "./../ABIs/AICabalNFT.json";
+import Carousel, { next } from "react-material-ui-carousel";
+import { Paper, Button } from "@mui/material";
+import Typer from "./Typer.js";
 
 export default function MainBox() {
-  const { address, isConnected } = useAccount()
-  const [_image,setImage] = useState([{
-    id: -1,
-    src: `${exampleIMG.example}`
-  }])
-  const [counter, setCounter] = useState(0)
-  const [current, setCurrent] = useState(0)
-  const [generating, setGenerating] = useState(false)
-  const [terminalCounter, setTerminalCounter] = useState(0)
-  const [deployed, setDeployed] = useState(false)
-  const [transactionH, setTransactionH] = useState("")
+	const { address, isConnected } = useAccount();
+	const [_image, setImage] = useState([
+		{
+			id: -1,
+			src: `${exampleIMG.example}`,
+		},
+	]);
+	const [counter, setCounter] = useState(0);
+	const [current, setCurrent] = useState(0);
+	const [generating, setGenerating] = useState(false);
+	const [terminalCounter, setTerminalCounter] = useState(0);
+	const [deployed, setDeployed] = useState(false);
+	const [transactionH, setTransactionH] = useState("");
 
+	function dataURLtoFile(dataurl, filename) {
+		var arr = dataurl.split(","),
+			mime = arr[0].match(/:(.*?);/)[1],
+			bstr = atob(arr[1]),
+			n = bstr.length,
+			u8arr = new Uint8Array(n);
 
-    
+		while (n--) {
+			u8arr[n] = bstr.charCodeAt(n);
+		}
 
-    function dataURLtoFile(dataurl, filename) {
- 
-      var arr = dataurl.split(','),
-          mime = arr[0].match(/:(.*?);/)[1],
-          bstr = atob(arr[1]), 
-          n = bstr.length, 
-          u8arr = new Uint8Array(n);
-          
-      while(n--){
-          u8arr[n] = bstr.charCodeAt(n);
-      }
-      
-      return new File([u8arr], filename, {type: "image/jpeg"});
-    }
+		return new File([u8arr], filename, { type: "image/jpeg" });
+	}
 
-  
+	async function storeImage() {
+		const client = new NFTStorage({
+			token: process.env.REACT_APP_NFT_STORAGE_API_KEY,
+		});
 
-    async function storeImage() {
+		try {
+			const d = await dataURLtoFile(
+				`data:image/jpeg;base64,${_image[current].src}`,
+				"cabalLabs.jpeg"
+			);
 
-      const client = new NFTStorage({
-        token: process.env.REACT_APP_NFT_STORAGE_API_KEY,
-      });
+			await client
+				.store({
+					image: d,
+					name: `Cabal Labs AI Generated NFT: ${address}`,
+					description:
+						"This NFT is created just for you, it uses your wallet addres to come up with art. Welcome to the Cabal...",
+				})
+				.then(async (metadata) => {
+					let metadataArray = metadata.url.split("/");
+					const provider = new ethers.providers.Web3Provider(window.ethereum);
+					const signer = provider.getSigner();
 
-      try {
-        const d = await dataURLtoFile(`data:image/jpeg;base64,${_image[current].src}`, "cabalLabs.jpeg");
+					let contract = new ethers.Contract(
+						process.env.REACT_APP_CONTRACT_ADD,
+						AICabalNFT,
+						signer
+					);
 
-        await client.store({
-          image: d,
-          name:`Cabal Labs AI Generated NFT: ${address}`,
-          description:
-          "This NFT is created just for you, it uses your wallet addres to come up with art. Welcome to the Cabal...",
-        }).then( async (metadata) => {
-          let metadataArray = metadata.url.split("/")
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner()
+					const price = await contract.salePrice();
+					const mint = await contract.mintNFT(
+						`https://${metadataArray[2]}.ipfs.w3s.link/${metadataArray[3]}`,
+						{ value: price }
+					);
 
-          let contract= new ethers.Contract(process.env.REACT_APP_CONTRACT_ADD,AICabalNFT, signer)
+					console.log("metadata saved: ", mint.hash);
+					setTransactionH(mint.hash);
+					setDeployed(true);
+				});
+		} catch (error) {
+			console.log(" could not safe NFT ", error);
+		}
+	}
 
-          const price = await contract.salePrice();
-          const mint = await contract.mintNFT(`https://${metadataArray[2]}.ipfs.w3s.link/${metadataArray[3]}`,{value: price})
+	async function AIimage() {
+		setGenerating(true);
+		fetch(process.env.REACT_APP_API_ADD + address)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				const img = data.image;
+				if (counter < 1) {
+					setImage((first) =>
+						first.filter((first) => {
+							return first.id !== -1;
+						})
+					);
+				}
+				setImage((_image) => [..._image, { id: counter, src: img[0] }]);
+				setCounter(counter + 1);
+				setGenerating(false);
+			});
 
-          console.log("metadata saved: ", mint.hash)
-          setTransactionH(mint.hash)
-          setDeployed(true)
-        })
-      } catch(error) {
-        console.log(" could not safe NFT ", error)
-      }
+		console.log(generating);
+	}
 
-    }
+	const onClick = () => {
+		console.log(_image[current]);
+		storeImage();
+	};
+	return (
+		<div className="mainCard">
+			<div className="picture-container">
+				<div className="innerBox">
+					<Carousel
+						autoPlay={false}
+						next={(next) => {
+							setCurrent(next);
+						}}
+						prev={(prev) => {
+							setCurrent(prev);
+						}}
+						navButtonsAlwaysVisible={true}>
+						{_image.map((image) => (
+							<img
+								src={`data:image/jpeg;base64,${image.src}`}
+								alt="Not Found"
+								className="img_style"
+							/>
+						))}
+					</Carousel>
+				</div>
 
-
-
-
-    async function AIimage() {
-      setGenerating(true)
-      fetch( process.env.REACT_APP_API_ADD + address).then(
-        res => res.json()
-      ).then(
-        data => {
-          console.log(data)
-          const img = data.image
-          if (counter < 1) {
-            setImage(first =>
-              first.filter(first => {
-                return first.id !== -1;
-              }),
-            );
-          }
-          setImage((_image) => [..._image,{id: counter, src: img[0] }])
-          setCounter(counter + 1)
-          setGenerating(false)
-        }
-      )
-      
-      console.log(generating)
-    }
-
-    const onClick = () => {
-      console.log(_image[current])
-      storeImage();
-    }
-    return (
-    <div className= "mainCard"> 
-      <div className="">
-            <div className="imageBox flex-col-hcenter">
-                <div className="innerBox">
-
-                <Carousel  
-                autoPlay={false}
-                next={ (next) => {setCurrent(next);} }
-                prev={ (prev) => {setCurrent(prev);} }
-                navButtonsAlwaysVisible={true}
-                >
-                {
-                _image.map( image => <img
-                src={`data:image/jpeg;base64,${image.src}`}
-                alt="Not Found"
-                className="img_style"
-                /> )
-                }
-              </Carousel>
-                </div>
-              
-                <div className="flex-row">
-                  { counter < 3 && isConnected
-                    ?<div  onClick={() => {AIimage()}} className="Button-Box">
-                    <p className="txt-614">Generate</p>
-                    </div>
-                    :
-                    <div className="Button-Box-disabled">
-                    <p className="txt-614">Generate</p>
-                    </div>
-                  }
-                    
-                    <div onClick={() => {onClick();}} className="Button-Box">
-                        <p className="txt-614">Mint NFT</p>
-                    </div>
-                </div>
-            </div>
-           
-          </div>
-           <div className="terminal">
-            <br></br>
-            {terminalCounter >=0 && <Typer text={" Welcome!"} terminalCounter={terminalCounter} setTerminalCounter={setTerminalCounter} increase={true}/>}
-            {terminalCounter >=1 && <Typer text={" We want to thank you for coming"} terminalCounter={terminalCounter} setTerminalCounter={setTerminalCounter} increase={true}/>}
-            {terminalCounter >=2 && <Typer text={" Here you can find a NFT minter that uses AI to generate an unique art piece for you"} terminalCounter={terminalCounter} setTerminalCounter={setTerminalCounter} increase={true}/>}
-            {(terminalCounter >=3 && !isConnected) && <Typer text={" Please connect you wallet..."} terminalCounter={terminalCounter} setTerminalCounter={setTerminalCounter} increase={false}/>}
-            {(terminalCounter >=3  && isConnected) &&   <Typer text={" You can generate up to 3 pieces, select the one you like and click Mint NFT"} terminalCounter={terminalCounter} setTerminalCounter={setTerminalCounter} increase={true} />}
-            {(terminalCounter >=4  && generating) &&   <Typer text={" Generating..."} terminalCounter={terminalCounter} setTerminalCounter={setTerminalCounter} increase={false} />}
-            {(deployed) && <Typer text={` Deployed! Transaction hash: ${transactionH} `} terminalCounter={terminalCounter} setTerminalCounter={setTerminalCounter} increase={true} />}
-          </div>
-
-      </div>
-    )
+				<div className="flex-row">
+					{counter < 3 && isConnected ? (
+						<div
+							onClick={() => {
+								AIimage();
+							}}
+							className="Button-Box">
+							<p className="txt-614">Generate</p>
+						</div>
+					) : (
+						<div className="Button-Box-disabled">
+							<p className="txt-614">Generate</p>
+						</div>
+					)}
+					<div
+						onClick={() => {
+							onClick();
+						}}
+						className="Button-Box">
+						<p className="txt-614">Mint NFT</p>
+					</div>
+				</div>
+			</div>
+			<div className="terminal directions">
+				<br></br>
+				{terminalCounter >= 0 && (
+					<Typer
+						text={" Welcome!"}
+						terminalCounter={terminalCounter}
+						setTerminalCounter={setTerminalCounter}
+						increase={true}
+					/>
+				)}
+				{terminalCounter >= 1 && (
+					<Typer
+						text={" We want to thank you for coming"}
+						terminalCounter={terminalCounter}
+						setTerminalCounter={setTerminalCounter}
+						increase={true}
+					/>
+				)}
+				{terminalCounter >= 2 && (
+					<Typer
+						text={
+							" Here you can find a NFT minter that uses AI to generate an unique art piece for you"
+						}
+						terminalCounter={terminalCounter}
+						setTerminalCounter={setTerminalCounter}
+						increase={true}
+					/>
+				)}
+				{terminalCounter >= 3 && !isConnected && (
+					<Typer
+						text={" Please connect you wallet..."}
+						terminalCounter={terminalCounter}
+						setTerminalCounter={setTerminalCounter}
+						increase={false}
+					/>
+				)}
+				{terminalCounter >= 3 && isConnected && (
+					<Typer
+						text={
+							" You can generate up to 3 pieces, select the one you like and click Mint NFT"
+						}
+						terminalCounter={terminalCounter}
+						setTerminalCounter={setTerminalCounter}
+						increase={true}
+					/>
+				)}
+				{terminalCounter >= 4 && generating && (
+					<Typer
+						text={" Generating..."}
+						terminalCounter={terminalCounter}
+						setTerminalCounter={setTerminalCounter}
+						increase={false}
+					/>
+				)}
+				{deployed && (
+					<Typer
+						text={` Deployed! Transaction hash: ${transactionH} `}
+						terminalCounter={terminalCounter}
+						setTerminalCounter={setTerminalCounter}
+						increase={true}
+					/>
+				)}
+			</div>
+		</div>
+	);
 }
